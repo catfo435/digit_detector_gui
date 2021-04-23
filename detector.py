3b62ac8b9bddc4341e0e05b52810a4fba3011c66
Pranav <74195851+PranavP435@users.noreply.github.com>
2021-04-23 10:37:11 +0530

    working code

--- detector.py
+++ detector.py
@@ -1,12 +1,13 @@
-from tkinter import *
+from tkinter import Tk, Canvas, Label, Button, mainloop, BOTH, YES, BOTTOM
 from PIL import Image, ImageDraw
-from tensorflow import keras
+from tensorflow import keras, autograph
 import numpy as np
+from skimage import io, transform
 
 canvas_width = 500
 canvas_height = 500
 
-cv_img = Image.new("RGB", (canvas_width, canvas_height), (255, 255, 255))
+cv_img = Image.new("RGB", (canvas_width, canvas_height), (0 ,0 ,0))
 draw = ImageDraw.Draw(cv_img)
 model = keras.models.load_model("model.h5")
 
@@ -18,12 +19,13 @@ def paint(event):
 
 	draw.ellipse([x1, y1, x2, y2], (255, 255, 255))
 
-
+@autograph.experimental.do_not_convert # IDK what this means, tensorflow warned me about something and asked me to do this, so I DID.
 def predict():
 	#image pre-processing
+	cv_img.save("image.jpg")
 	np_img = np.asarray(cv_img)
 	gray = np.dot(np_img[...,:], [0.299, 0.587, 0.114])  
-	gray28x28 = transform.resize(gray, (img_rows, img_cols))  
+	gray28x28 = transform.resize(gray, (28, 28))  
 	vectorized_filter = np.vectorize(lambda v: 255 if v > 128 else v)  
 	filtered = vectorized_filter(gray28x28)
 	batch = np.array([filtered])
@@ -35,8 +37,9 @@ def clear():
 	global cv_img, draw
 
 	cv.delete("all")
-	cv_img = Image.new("RGB", (canvas_width, canvas_height), (255, 255, 255))
+	cv_img = Image.new("RGB", (canvas_width, canvas_height), (0 ,0, 0))
 	draw = ImageDraw.Draw(cv_img)
+	prediction_message.configure(text="Prediction = None")
 
 
 master = Tk()
@@ -48,7 +51,7 @@ cv.bind("<B1-Motion>", paint)
 message = Label(master, text="Press and Drag the mouse to draw")
 message.pack(side=BOTTOM)
 
-prediction_message = Label(master, text="Prediction = None")
+prediction_message = Label(master, text="Prediction = None", font=("Arial", 25))
 prediction_message.pack(side=BOTTOM)
 
 predict_button = Button(master, text= "Predict", command = predict)
